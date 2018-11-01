651d8c7caf274e51d6d830a3b2ade0ce712c3658
David Tsai <david@whataniceguy.com>
2018-10-31 18:06:40 -0700

    Adding react and react-dom as peer deps, and golden-layout

--- src/golden-layout-react.ts
+++ src/golden-layout-react.ts
@@ -1,6 +1,5 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
-
-}
+// import "core-js/fn/array.find"
+// ...
+import { Component } from 'react'
+export default class DummyClass {}
